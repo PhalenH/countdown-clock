d0497ecdf7e89fbfd71208f9b845cec94508c085
Phalen Hazel <88113752+PhalenH@users.noreply.github.com>
2022-04-13 16:28:09 -0400

    added stateful variables to calender home.js add styling for datepicker package

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,7 @@ import getDay from "date-fns/getDay";
 import parse from "date-fns/parse";
 import startOfWeek from "date-fns/startOfWeek";
 import "react-big-calendar/lib/css/react-big-calendar.css";
+import "react-datepicker/dist/react-datepicker.css";
 
 const locales = {
   "en-US": require("date-fns/locale/en-US"),
@@ -21,30 +22,58 @@ const localizer = dateFnsLocalizer({
 const events = [
   {
     title: "Job stuff",
-    start: new Date(2022, 2, 1),
-    end: new Date(2022, 2, 2),
+    start: new Date(2022, 3, 1),
+    end: new Date(2022, 3, 2),
   },
   {
     title: "Deep meditation",
-    start: new Date(2022, 2, 25),
-    end: new Date(2022, 2, 25),
+    start: new Date(2022, 3, 25),
+    end: new Date(2022, 3, 25),
   },
   {
     title: "Girlfriend things",
     allDay: true,
-    start: new Date(2022, 2, 22),
-    end: new Date(2022, 2, 24),
+    start: new Date(2022, 3, 22),
+    end: new Date(2022, 3, 24),
   },
 ];
 const Home = () => {
+  const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
+  const [allEvents, setAllEvents] = useState(events);
+
+  const handleAddEvent = () => {
+    setAllEvents(...allEvents, newEvent);
+  };
+
   return (
     <div className="calendar">
+      <header>Calendar</header>
+      <h1>Add New Event</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Add Name"
+          value={newEvent.title}
+          onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })}
+        />
+        <DatePicker
+          placeholder="Start Date"
+          selected={newEvent.start}
+          onChange={(start) => setNewEvent({ ...newEvent, start })}
+        />
+        <DatePicker
+          placeholder="End Date"
+          selected={newEvent.end}
+          onChange={(end) => setNewEvent({ ...newEvent, end })}
+        />
+        <button onClick={handleAddEvent}>Add Event</button>
+      </div>
       <Calendar
         localizer={localizer}
-        events={events}
+        events={allEvents}
         startAccessor="start"
         endAccessor="end"
-        style={{height: 500, margin: "50px"}}
+        style={{ height: 500, margin: "50px" }}
       ></Calendar>
     </div>
   );
